aac14096a6698420e2c8d5fcf2b45abab0580233
Dineshyvss <dinesh.yvss@gmail.com>
2023-05-28 16:32:21 -0500

    menu bar update

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,3 +1,34 @@
+<script setup>
+import ocLogo from "/oc_logo.png";
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import UserServices from "../services/UserServices";
+
+const router = useRouter();
+
+const user = ref(null);
+const title = ref("Travel Iternary");
+const logoURL = ref("");
+
+onMounted(() => {
+  logoURL.value = ocLogo;
+  user.value = JSON.parse(localStorage.getItem("user"));
+});
+
+function logout() {
+  UserServices.logoutUser()
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  localStorage.removeItem("user");
+  user.value = null;
+  router.push({ name: "login" });
+}
+</script>
+
 <template>
   <div>
     <v-app-bar color="primary" app dark>
@@ -14,14 +45,19 @@
         {{ title }}
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn class="mx-2" :to="{ name: 'recipes' }">Recipes</v-btn>
-      <v-btn v-if="user === null" class="mx-2" :to="{ name: 'login' }">
-        Login
+      <v-btn class="mx-2" :to="{ name: 'home' }">Home</v-btn>
+      <v-btn class="mx-2" :to="{ name: 'recipes' }">Tours</v-btn>
+      <v-btn v-if="user !== null" class="mx-2" :to="{ name: 'subscribe' }">Subscribers</v-btn>
+      
+      <v-btn v-if="user === null" class="mx-2" :to="{ name: 'about' }">
+        About
       </v-btn>
-      <v-btn v-if="user !== null" class="mx-2" :to="{ name: 'ingredients' }">
-        Ingredients
-      </v-btn>
-      <v-menu v-if="user !== null" min-width="200px" rounded>
+      <v-btn v-if="user === null" class="mx-2" :to="{ name: 'book' }">Subscribe</v-btn>
+      <v-btn class="mx-2" :to="{ name: 'login' }">Login</v-btn>
+
+
+     
+            <v-menu v-if="user !== null" min-width="200px" rounded>
         <template v-slot:activator="{ props }">
           <v-btn icon v-bind="props">
             <v-avatar class="mx-auto text-center" color="accent" size="large">
@@ -44,7 +80,7 @@
                 {{ user.email }}
               </p>
               <v-divider class="my-3"></v-divider>
-              <v-btn rounded variant="text" @click="logout">Logout</v-btn>
+              <v-btn rounded variant="text" @click="logout()"> Logout </v-btn>
             </div>
           </v-card-text>
         </v-card>
@@ -52,45 +88,3 @@
     </v-app-bar>
   </div>
 </template>
-
-<script>
-import ocLogo from "/oc_logo.png";
-import { ref, onMounted } from "vue";
-import { useRouter } from "vue-router";
-import UserServices from "../services/UserServices";
-
-export default {
-  setup() {
-    const router = useRouter();
-
-    const user = ref(null);
-    const title = ref("Recipes");
-    const logoURL = ref("");
-
-    onMounted(() => {
-      logoURL.value = ocLogo;
-      user.value = JSON.parse(localStorage.getItem("user"));
-    });
-
-    function logout() {
-      UserServices.logoutUser()
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-      localStorage.removeItem("user");
-      user.value = null;
-      router.push({ name: "login" });
-    }
-
-    return {
-      user,
-      title,
-      logoURL,
-      logout,
-    };
-  },
-};
-</script>
