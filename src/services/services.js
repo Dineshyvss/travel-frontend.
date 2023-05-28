50b86f52ede86c47bb4faa107459c72fd063a937
Dineshyvss <dinesh.yvss@gmail.com>
2023-05-28 17:05:41 -0500

    sever.js file updated

--- src/services/services.js
+++ src/services/services.js
@@ -1,21 +1,9 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 const baseURL = process.env.NODE_ENV === "development" ? "http://localhost/travelapi/" : "/travelapi/";
 
 const apiClient = axios.create({
   baseURL: baseURL,
-=======
-var baseurl = "";
-if (process.env.NODE_ENV === "development") {
-  baseurl = "http://http://ec2-3-94-57-116.compute-1.amazonaws.com/travelapi/";
-} else {
-  baseurl = "/travelapi/";
-}
-
-const apiClient = axios.create({
-  baseURL: baseurl,
->>>>>>> origin/deploy_travel
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
--- src/views/EditRecipe.vue
+++ src/views/EditRecipe.vue
@@ -317,20 +317,12 @@ function closeSnackBar() {
                 ></v-text-field>
                 <v-text-field
                   v-model.number="recipe.servings"
-<<<<<<< HEAD
                   label="Enter The price"
-=======
-                  label="Number of Servings"
->>>>>>> origin/deploy_travel
                   type="number"
                 ></v-text-field>
                 <v-text-field
                   v-model.number="recipe.time"
-<<<<<<< HEAD
                   label="Number of days (in days)"
-=======
-                  label="Time to Make (in minutes)"
->>>>>>> origin/deploy_travel
                   type="number"
                 ></v-text-field>
                 <v-switch
@@ -351,11 +343,7 @@ function closeSnackBar() {
           </v-card-text>
           <v-card-actions class="pt-0">
             <v-btn variant="flat" color="primary" @click="updateRecipe()"
-<<<<<<< HEAD
               >Update Tour</v-btn
-=======
-              >Update Recipe</v-btn
->>>>>>> origin/deploy_travel
             >
             <v-spacer></v-spacer>
           </v-card-actions>
@@ -368,11 +356,7 @@ function closeSnackBar() {
           <v-card-title
             ><v-row align="center">
               <v-col cols="10"
-<<<<<<< HEAD
                 ><v-card-title class="headline">Attractions </v-card-title>
-=======
-                ><v-card-title class="headline">Ingredients </v-card-title>
->>>>>>> origin/deploy_travel
               </v-col>
               <v-col class="d-flex justify-end" cols="2">
                 <v-btn color="accent" @click="openAddIngredient()">Add</v-btn>
@@ -476,20 +460,13 @@ function closeSnackBar() {
       <v-card class="rounded-lg elevation-5">
         <v-card-title class="headline mb-2">{{
           isAddIngredient
-<<<<<<< HEAD
             ? "Add Attractions"
             : isEditIngredient
             ? "Edit Attractions"
-=======
-            ? "Add Ingredient"
-            : isEditIngredient
-            ? "Edit Ingredient"
->>>>>>> origin/deploy_travel
             : ""
         }}</v-card-title>
         <v-card-text>
           <v-row>
-<<<<<<< HEAD
           
 
             <v-col>
@@ -499,25 +476,6 @@ function closeSnackBar() {
                 item-title="name"
                 item-value="unit"
                 label="Attractions"
-=======
-            <v-col cols="3">
-              <v-text-field
-                v-model="newIngredient.quantity"
-                label="Quantity"
-                type="number"
-                required
-              >
-              </v-text-field>
-            </v-col>
-
-            <v-col>
-              <v-select
-                v-model="selectedIngredient"
-                :items="ingredients"
-                item-title="name"
-                item-value="unit"
-                label="Ingredients"
->>>>>>> origin/deploy_travel
                 return-object
                 required
               >
@@ -595,11 +553,7 @@ function closeSnackBar() {
             :items="recipeIngredients"
             item-title="ingredient.name"
             item-value="id"
-<<<<<<< HEAD
             label="Attractions"
-=======
-            label="Ingredients"
->>>>>>> origin/deploy_travel
             return-object
             multiple
             chips
