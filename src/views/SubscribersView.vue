7ff746ea4d5ce56d1df3904b28647d9dcd0633ab
Dineshyvss <dinesh.yvss@gmail.com>
2023-05-28 16:33:19 -0500

    routs update

--- src/views/SubscribersView.vue
+++ src/views/SubscribersView.vue
@@ -1,3 +1,58 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "login",
+      component: () => import("./views/Login.vue"),
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: () => import("./views/AboutView.vue"),
+    },
+    {
+      path: "/subscribe",
+      name: "subscribe",
+      component: () => import("./views/SubscribersView.vue"),
+    },
+    {
+      path: "/tours",
+      name: "tours",
+      component: () => import("./views/ToursWeoffer.vue"),
+    },
+    {
+      path: "/book",
+      name: "book",
+      component: () => import("./views/subscribe.vue"),
+    },
+    {
+      path: "/recipes",
+      name: "recipes",
+      component: () => import("./views/RecipeList.vue"),
+    },
+    {
+      path: "/home",
+      name: "home",
+      component: () => import("./views/HomeView.vue"),
+    },
+    {
+      path: "/recipe/:id",
+      name: "editRecipe",
+      props: true,
+      component: () => import("./views/EditRecipe.vue"),
+    },
+    {
+      path: "/ingredients",
+      name: "ingredients",
+      component: () => import("./views/IngredientList.vue"),
+    },
+  ],
+});
+
+export default router;
 <template>
   <v-container>
     <div id="body">
