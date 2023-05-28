5acc608ff1dacbb0121fc75ba748474e29a7bea3
Dineshyvss <dinesh.yvss@gmail.com>
2023-05-28 15:52:46 -0500

    new commit with subscrib updates

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,20 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import dns from "dns";
 dns.setDefaultResultOrder("verbatim");
+// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from "vite-plugin-vuetify";
 
-export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })],
+export default () => {
+  const baseURL =
+    process.env.APP_ENV === "development" ? "/" : "/recipe-frontend/";
 
-  server: {
-    host: "0.0.0.0", // Set the host to "0.0.0.0" to make the app accessible externally
-    port: 8081,
-  },
-  base: "/", // Update the base URL to "/"
-});
+  return defineConfig({
+    plugins: [vue(), vuetify({ autoImport: true })],
+
+    server: {
+      host: "ec2-3-94-57-116.compute-1.amazonaws.com",
+      port: 8081,
+    },
+    base: baseURL,
+  });
+};
