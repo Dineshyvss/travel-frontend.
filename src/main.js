e1c9f932c8de6238d14b162b911b33823a7baced
dineshyvss <130199434+Dineshyvss@users.noreply.github.com>
2023-06-01 13:42:37 -0500

    Delete src directory

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router.js";
-import vuetify from "./plugins/vuetify";
-import { loadFonts } from "./plugins/webfontloader";
-
-loadFonts();
-
-createApp(App).use(vuetify).use(router).mount("#app");
